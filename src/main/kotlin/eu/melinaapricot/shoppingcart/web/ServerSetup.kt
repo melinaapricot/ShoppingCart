6c36355d0247296108088b7e7c30b06b4a2eac54
Mavridis,Alkis <Alkis.Mavridis@drei.com>
2020-11-14 22:53:44 +0100

    Setup corse parameters

--- src/main/kotlin/eu/melinaapricot/shoppingcart/web/ServerSetup.kt
+++ src/main/kotlin/eu/melinaapricot/shoppingcart/web/ServerSetup.kt
@@ -4,12 +4,27 @@ import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.CORS
 import io.ktor.features.ContentNegotiation
+import io.ktor.features.maxAge
 import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import io.ktor.jackson.JacksonConverter
 
 fun Application.setup() {
     install(ContentNegotiation) {
         register(ContentType.Application.Json, JacksonConverter())
     }
-    install(CORS)
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Get)
+        method(HttpMethod.Post)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.AccessControlAllowHeaders)
+        header(HttpHeaders.ContentType)
+        header(HttpHeaders.AccessControlAllowOrigin)
+        allowCredentials = true
+        anyHost()
+    }
 }
