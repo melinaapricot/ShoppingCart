69283ac0733bafa54c077afeeaf804c9be948daa
Mavridis,Alkis <Alkis.Mavridis@drei.com>
2020-11-15 21:40:16 +0100

    Setup logging

--- src/main/kotlin/eu/melinaapricot/shoppingcart/web/ServerSetup.kt
+++ src/main/kotlin/eu/melinaapricot/shoppingcart/web/ServerSetup.kt
@@ -4,16 +4,15 @@ import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.CORS
 import io.ktor.features.ContentNegotiation
-import io.ktor.features.maxAge
-import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
-import io.ktor.jackson.JacksonConverter
+import io.ktor.jackson.jackson
 
 fun Application.setup() {
     install(ContentNegotiation) {
-        register(ContentType.Application.Json, JacksonConverter())
+        jackson {  }
     }
+
     install(CORS) {
         method(HttpMethod.Options)
         method(HttpMethod.Get)
